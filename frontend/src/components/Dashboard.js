2577be1a29811b2d9b0b0e2873aeffb006e84b01
VigneshHariharan <phvigneshariharan@gmail.com>
2019-09-10 19:15:50 +0530

    Added reactstrap

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 export class Dashboard extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Dashboard</h1>
-				{/* Add a Title Bar here */}
+				<Container>
+					<Row>
+						<Col>{/* Add a Title Bar here */}</Col>
+					</Row>
+
+					<Row>
+						<Col>Title</Col>
+						<Col>Title</Col>
+						<Col>Title</Col>
+					</Row>
+				</Container>
 			</div>
 		);
 	}
