2505d8d821c7334d700714955a9728cf6a09b8a3
VigneshHariharan <phvigneshariharan@gmail.com>
2019-09-13 12:15:34 +0530

    build added

---
+++ build/precache-manifest.d22566ad1e80cdbe359f418f6c94ba66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b774dc1786b17753bae6b554e21a64c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d8d29190265afbf24a",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "7be3dbe1b613979d69ef",
+    "url": "/static/css/main.a4b6da65.chunk.css"
+  },
+  {
+    "revision": "05d8d29190265afbf24a",
+    "url": "/static/js/2.087ac8b0.chunk.js"
+  },
+  {
+    "revision": "7be3dbe1b613979d69ef",
+    "url": "/static/js/main.438442a9.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
